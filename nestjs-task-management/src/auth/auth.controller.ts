f5ef37368f634b0935b81296a03f4785214e7685
softdev <softdev1988@gmail.com>
2020-09-04 00:23:41 +0530

    Section 5

--- nestjs-task-management/src/auth/auth.controller.ts
+++ nestjs-task-management/src/auth/auth.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, UseGuards, Req } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user-decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    /**
+     *
+     */
+    constructor(
+        private authService: AuthService
+    ) { }
+    @Post('/signup')
+    signUp(@Body(ValidationPipe) authCredentialDto : AuthCredentialsDto):Promise<void>{
+        return this.authService.signUp(authCredentialDto);
+    }
+
+    @Post('/signin')
+    signIn(@Body(ValidationPipe) authCredentialDto : AuthCredentialsDto):Promise<{accessToken: string}>{
+        return this.authService.signIn(authCredentialDto);
+    }
+}
+
+
